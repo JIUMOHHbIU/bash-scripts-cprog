02e68e1c1786dc874bf98ef8555b4528d0064891
JIUMOHHbIU <anto.zhil.anto.zhil@gmail.com>
2024-03-21 00:03:37 +0300

    ✨ removed bs

--- build_debug.sh
+++ build_debug.sh
@@ -2,34 +2,11 @@
 
 gcc_compile_args=("-std=c99" "-Wall" "-Werror" "-Wpedantic" "-Wextra" "-Wfloat-equal" "-Wfloat-conversion" "-Wvla" "-fdiagnostics-color")
 
-obj_suffix=".o"
-for source_file in *.c; do
-	o_file="${source_file//.c/$obj_suffix}"
-	if [ -f "$o_file" ]; then
-		o_modification_date=$(date -r "$o_file" +%s)
-		c_modification_date=$(date -r "$source_file" +%s)
-		last_modification_date="$c_modification_date"
+if ! gcc "${gcc_compile_args[@]}" -g -DDEBUG -c -fprofile-arcs -ftest-coverage -O0 ./*.c; then
+	exit 1
+fi
 
-		h_file="${source_file//.c/.h}"
-		if [ -f "$h_file" ]; then
-			h_modification_date=$(date -r "$h_file" +%s)
-
-			if [ "$h_modification_date" -gt "$last_modification_date" ]; then
-				last_modification_date="$h_modification_date"
-			fi
-		fi
-
-		if [ "$last_modification_date" -lt "$o_modification_date" ]; then
-			continue
-		fi
-	fi
-
-	if ! gcc "${gcc_compile_args[@]}" -g -DDEBUG -c -fprofile-arcs -ftest-coverage -O0 "$source_file" -o "$o_file"; then
-		exit 1
-	fi
-done
-
-if ! gcc -fprofile-arcs -o app.exe ./*"$obj_suffix" -lm; then
+if ! gcc -fprofile-arcs -o app.exe ./*.o -lm; then
 	exit 1
 fi
 
--- build_debug_msan.sh
+++ build_debug_msan.sh
@@ -1,39 +1,16 @@
 #!/bin/bash
 
-obj_suffix=".debug_msan.o"
-for source_file in *.c; do
-	o_file="${source_file//.c/$obj_suffix}"
-	if [ -f "$o_file" ]; then
-		o_modification_date=$(date -r "$o_file" +%s)
-		c_modification_date=$(date -r "$source_file" +%s)
-		last_modification_date="$c_modification_date"
+if ! clang-17 -c -std=c99 -Weverything \
+	-Wno-used-but-marked-unused \
+	-fcolor-diagnostics \
+	-Wno-declaration-after-statement \
+	-Wno-unsafe-buffer-usage \
+	-Wno-missing-prototypes \
+	-DDEBUG -fsanitize=memory -fno-omit-frame-pointer -fno-optimize-sibling-calls -O1 -fPIE -g ./*.c; then
+	exit 1
+fi
 
-		h_file="${source_file//.c/.h}"
-		if [ -f "$h_file" ]; then
-			h_modification_date=$(date -r "$h_file" +%s)
-
-			if [ "$h_modification_date" -gt "$last_modification_date" ]; then
-				last_modification_date="$h_modification_date"
-			fi
-		fi
-
-		if [ "$last_modification_date" -lt "$o_modification_date" ]; then
-			continue
-		fi
-	fi
-
-	if ! clang-17 -c -std=c99 -Weverything \
-		-Wno-used-but-marked-unused \
-		-fcolor-diagnostics \
-		-Wno-declaration-after-statement \
-		-Wno-unsafe-buffer-usage \
-		-Wno-missing-prototypes \
-		-DDEBUG -fsanitize=memory -fno-omit-frame-pointer -fno-optimize-sibling-calls -O1 -fPIE -g "$source_file" -o "$o_file"; then
-		exit 1
-	fi
-done
-
-if ! clang -fsanitize=memory -pie ./*"$obj_suffix" -o app.exe; then
+if ! clang -fsanitize=memory -pie ./*.o -o app.exe; then
 	exit 1
 fi
 
